f7591ceb9836cfe8f9879440289c026ef228f72c
Bram Schuur <bram@typlab.com>
2013-01-18 16:57:17 +0100

    Added table generation for records.

---
+++ src/DB/Flex/Create.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE TemplateHaskell, GADTs, TypeOperators, Rank2Types #-}
+module DB.Flex.Create where
+
+import Data.Char
+import Data.Label.Util
+import Data.Maybe
+import Database.HDBC
+
+import DB.Flex.Config
+import DB.Flex.Monad
+import DB.Flex.Record
+
+import Language.Haskell.TH
+import Language.Haskell.TH.Util
+
+data FieldOpt a where
+  Nullable :: FieldOpt (Maybe a)
+  Default  :: a -> FieldOpt a
+  Primary  :: FieldOpt a
+  Unique   :: FieldOpt a
+  Foreign  :: DBTable t => t |> a -> FieldOpt a
+
+data FieldOpts a = FieldOpts { fieldOpts :: [FieldOpt a] }
+
+-- | Generate table instance from a datatype
+
+mkTable :: (String -> String) -> Name -> Q [Dec]
+mkTable f = withTyConReify $ mkTable' f
+
+mkTable' :: (String -> String) -> Dec -> Q [Dec]
+mkTable' mkId (DataD _ tnm pars [RecC cns fs] _) =
+     let ps = reverse $ tail $ reverse $ map getTV pars
+         fieldNs = map (\(n,_,_) -> mkId (show n)) fs 
+         inst = InstanceD [] (ConT (mkName "Functor1") `AppT` (foldl AppT (ConT tnm) $ map VarT ps))
+                   [FunD (mkName "tableName") [Clause [WildP] (NormalB $ LitE $ StringL $ mkId $ show tnm) []]
+                   ,FunD (mkName "fieldNames")
+                       [Clause [] (NormalB $ foldl AppE (ConE cns) $ map (AppE (ConE $ mkName "FieldName") . LitE . StringL) fieldNs) []]
+                   ]
+     in return $ [ inst ]
+mkTable' _ _ = error "Not a valid datatype in mkTable'"
+
+-- | Generate a table datatype from a remote Postgres database
+
+retrieveTable :: String -> String -> (String -> String) -> (SqlTypeId -> String) -> Config -> Q [Dec]
+retrieveTable tbl hnm mkI mkT conf =
+  do cols <- runIO $ withConnection conf $ flip describeTable tbl
+     let mkCol (col, cDesc) = addNullable (fromMaybe True $ colNullable cDesc)
+                                      (mkName $ mkI col, NotStrict, ConT $ mkName $ mkT $ colType cDesc)
+         addNullable True  (n, s, t) = (n, s, AppT (ConT $ mkName "Maybe") t)
+         addNullable False v         = v
+     let dat  = DataD [] (mkName hnm) [] [RecC (mkName hnm) (map mkCol cols)] [mkName "Show", mkName "Eq"]
+     rec <- dbRecord' dat
+     let anm = (\(DataD _ v _ _ _) -> v) $ head rec
+         inst = InstanceD [] (ConT (mkName "DBTable") `AppT` ConT anm)
+                   [FunD (mkName "tableName") [Clause [WildP] (NormalB $ LitE $ StringL tbl) []]
+                   ,FunD (mkName "fieldNames")
+                       [Clause [] (NormalB $ foldl AppE (ConE anm) $ map (AppE (ConE $ mkName "FieldName") . LitE . StringL) $ map fst cols) []]
+                   ]
+     return $ [ dat, inst ] ++ rec
+
+-- | Mapping SQL types to Haskell types
+baseTypeInfo :: SqlTypeId -> String
+baseTypeInfo SqlCharT              = "String"     
+baseTypeInfo SqlVarCharT           = "String"     
+baseTypeInfo SqlLongVarCharT       = "String"     
+baseTypeInfo SqlWCharT             = "String"     
+baseTypeInfo SqlWVarCharT          = "String"     
+baseTypeInfo SqlWLongVarCharT      = "String"     
+baseTypeInfo SqlDecimalT           = "Integer"    
+baseTypeInfo SqlNumericT           = "Integer" 
+baseTypeInfo SqlSmallIntT          = "Int"     
+baseTypeInfo SqlIntegerT           = "Int"     
+baseTypeInfo SqlRealT              = "Double"
+baseTypeInfo SqlFloatT             = "Double"  
+baseTypeInfo SqlDoubleT            = "Double"  
+baseTypeInfo SqlBitT               = "Bool"    
+baseTypeInfo SqlTinyIntT           = "Int"     
+baseTypeInfo SqlBigIntT            = "Integer"    
+baseTypeInfo SqlBinaryT            = "ByteString" 
+baseTypeInfo SqlVarBinaryT         = "ByteString" 
+baseTypeInfo SqlLongVarBinaryT     = "ByteString" 
+baseTypeInfo SqlDateT              = "UTCTime"    
+baseTypeInfo SqlTimeT              = "UTCTime"    
+baseTypeInfo SqlTimeWithZoneT      = "UTCTime"    
+baseTypeInfo SqlTimestampT         = "UTCTime"    
+baseTypeInfo SqlTimestampWithZoneT = "UTCTime"    
+baseTypeInfo SqlUTCDateTimeT       = "UTCTime"    
+baseTypeInfo SqlUTCTimeT           = "UTCTime"    
+baseTypeInfo (SqlIntervalT _)      = "SqlValue" 
+baseTypeInfo SqlGUIDT              = "UUID"     
+baseTypeInfo (SqlUnknownT "2950")  = "UUID"
+baseTypeInfo (SqlUnknownT _)       = "SqlValue" 
+
+withCase :: Eq a => a -> r -> (a -> r) -> a -> r
+withCase a r f a' | a' == a = r
+                  | otherwise = f a'
+
+baseIdent :: String -> String
+baseIdent = ("_" ++) . firstDown . mkData
+
+firstDown :: String -> String
+firstDown [] = []
+firstDown (x:xs) = toLower x: xs
+
+mkData :: String -> String
+mkData ('_' : x : xs) = toUpper x : mkData xs
+mkData (x : xs) | isAlphaNum x = x : mkData xs
+                | otherwise    = mkData xs
+mkData a = a
+
+mkField :: String -> String
+mkField (x:xs) | isAlphaNum x || x == '_' = toLower x : mkField xs
+               | otherwise                = mkField xs
+mkField a = a
--- src/DB/Flex/Record.hs
+++ src/DB/Flex/Record.hs
@@ -23,10 +23,7 @@ module DB.Flex.Record where
 
 import Control.Monad.Identity
 
-import Data.Char
 import Data.Convertible
--- import Data.Label.Util
-import Data.Maybe
 import Database.HDBC
 
 import Data.Record.Abstract
@@ -35,18 +32,10 @@ import Data.Functor1
 import Data.Traversable1
 import Data.Zippable1
 
-import DB.Flex.Config
-import DB.Flex.Monad
-
 import Language.Haskell.TH
 import Language.Haskell.TH.Util
-{-
-newtype Lens1 t a = Lens1 { unLens1 :: forall f. t f :-> f a }
-newtype Val1 (t :: * -> *) a = Val1 { unVal1 :: t a }
--}
 
 data Tup1 f g a = Tup1 (f a) (g a)
-
 
 -- * Database fields
 data FieldName a = FieldName { unFieldName :: String }
@@ -54,16 +43,6 @@ data FieldName a = FieldName { unFieldName :: String }
 data Field a where
   Field :: (Convertible a SqlValue, Convertible SqlValue a) => Field a
 
-{-
-data FieldOpt a where
-  Nullable :: FieldOpt (Maybe a)
-  Default  :: a -> FieldOpt a
-  Primary  :: FieldOpt a
-  Unique   :: FieldOpt a
-  Foreign  :: DBTable t => t |> a -> FieldOpt a
-
-data FieldOpts a = FieldOpts { fieldOpts :: [FieldOpt a] }
--}
 
 class (Zippable1 r, Traversable1 r) => DBRecord r where
   recordFields :: r Field
@@ -71,8 +50,6 @@ class (Zippable1 r, Traversable1 r) => DBRecord r where
 class DBRecord t => DBTable t where
   tableName    :: t v -> String
   fieldNames   :: t FieldName
---  fieldOptions :: t FieldOpts
---  fieldOptions = fmap1 (\_ -> FieldOpts []) recordFields
 
 instance (Convertible a SqlValue, Convertible SqlValue a) => DBRecord (AbstractVal a) where
   recordFields = AbstractVal Field
@@ -116,87 +93,3 @@ dbRecord' dc =
      zipp <- mkZippable1'    (head abst)
      flds <- mkRecordFields' (head abst)
      return $ abst ++ func ++ fold ++ trav ++ zipp ++ flds
-
-{-
--- | Generate table instance from a datatype
-
-mkTableFields :: Dec -> Q [Dec]
-mkTableFields dc =
-  do 
-     let anm = (\(DataD _ v _ _ _) -> v) $ head rec
-         inst = InstanceD [] (ConT (mkName "DBTable") `AppT` ConT anm)
-                   [FunD (mkName "tableName") [Clause [WildP] (NormalB $ LitE $ StringL tbl) []]
-                   ,FunD (mkName "fieldNames")
-                       [Clause [] (NormalB $ foldl AppE (ConE anm) $ map (AppE (ConE $ mkName "FieldName") . LitE . StringL) $ map fst cols) []]
-                   ]
-     return $ [ inst ]
--}
-
--- | Generate a table datatype from a remote Postgres database
-
-retrieveTable :: String -> String -> (String -> String) -> (SqlTypeId -> String) -> Config -> Q [Dec]
-retrieveTable tbl hnm mkI mkT conf =
-  do cols <- runIO $ withConnection conf $ flip describeTable tbl
-     let mkCol (col, cDesc) = addNullable (fromMaybe True $ colNullable cDesc)
-                                      (mkName $ mkI col, NotStrict, ConT $ mkName $ mkT $ colType cDesc)
-         addNullable True  (n, s, t) = (n, s, AppT (ConT $ mkName "Maybe") t)
-         addNullable False v         = v
-     let dat  = DataD [] (mkName hnm) [] [RecC (mkName hnm) (map mkCol cols)] [mkName "Show", mkName "Eq"]
-     rec <- dbRecord' dat
-     let anm = (\(DataD _ v _ _ _) -> v) $ head rec
-         inst = InstanceD [] (ConT (mkName "DBTable") `AppT` ConT anm)
-                   [FunD (mkName "tableName") [Clause [WildP] (NormalB $ LitE $ StringL tbl) []]
-                   ,FunD (mkName "fieldNames")
-                       [Clause [] (NormalB $ foldl AppE (ConE anm) $ map (AppE (ConE $ mkName "FieldName") . LitE . StringL) $ map fst cols) []]
-                   ]
-     return $ [ dat, inst ] ++ rec
-
--- | Mapping SQL types to Haskell types
-baseTypeInfo :: SqlTypeId -> String
-baseTypeInfo SqlCharT              = "String"     
-baseTypeInfo SqlVarCharT           = "String"     
-baseTypeInfo SqlLongVarCharT       = "String"     
-baseTypeInfo SqlWCharT             = "String"     
-baseTypeInfo SqlWVarCharT          = "String"     
-baseTypeInfo SqlWLongVarCharT      = "String"     
-baseTypeInfo SqlDecimalT           = "Integer"    
-baseTypeInfo SqlNumericT           = "Integer" 
-baseTypeInfo SqlSmallIntT          = "Int"     
-baseTypeInfo SqlIntegerT           = "Int"     
-baseTypeInfo SqlRealT              = "Double"
-baseTypeInfo SqlFloatT             = "Double"  
-baseTypeInfo SqlDoubleT            = "Double"  
-baseTypeInfo SqlBitT               = "Bool"    
-baseTypeInfo SqlTinyIntT           = "Int"     
-baseTypeInfo SqlBigIntT            = "Integer"    
-baseTypeInfo SqlBinaryT            = "ByteString" 
-baseTypeInfo SqlVarBinaryT         = "ByteString" 
-baseTypeInfo SqlLongVarBinaryT     = "ByteString" 
-baseTypeInfo SqlDateT              = "UTCTime"    
-baseTypeInfo SqlTimeT              = "UTCTime"    
-baseTypeInfo SqlTimeWithZoneT      = "UTCTime"    
-baseTypeInfo SqlTimestampT         = "UTCTime"    
-baseTypeInfo SqlTimestampWithZoneT = "UTCTime"    
-baseTypeInfo SqlUTCDateTimeT       = "UTCTime"    
-baseTypeInfo SqlUTCTimeT           = "UTCTime"    
-baseTypeInfo (SqlIntervalT _)      = "SqlValue" 
-baseTypeInfo SqlGUIDT              = "UUID"     
-baseTypeInfo (SqlUnknownT "2950")  = "UUID"
-baseTypeInfo (SqlUnknownT _)       = "SqlValue" 
-
-withCase :: Eq a => a -> r -> (a -> r) -> a -> r
-withCase a r f a' | a' == a = r
-                  | otherwise = f a'
-
-baseIdent :: String -> String
-baseIdent = ("_" ++) . firstDown . mkid
-
-firstDown :: String -> String
-firstDown [] = []
-firstDown (x:xs) = toLower x: xs
-
-mkid :: String -> String
-mkid "" = ""
-mkid ('_' : x : xs) = toUpper x : mkid xs
-mkid (x : xs) | isAlphaNum x = x : mkid xs
-              | otherwise    = mkid xs
